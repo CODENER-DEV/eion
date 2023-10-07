0569e211234b0febf8136336092d9432cff0b616
Domingo Hernandez Perez <ferrari075@hotmail.com>
2023-10-07 02:53:16 -0600

    update

--- src/settings/config.ts
+++
@@ -1,5 +0,0 @@
-export const config = {
-    urlFullSchedule: 'https://api.tvmaze.com/schedule/full',
-    urlShowSingleSearch: 'http://api.tvmaze.com/search/shows?q=',
-    urlRandomUser: 'https://randomuser.me/api'
-}
